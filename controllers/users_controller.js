4fe9f2601e15004e1b7d6b1bc98c3602503b4952
amanb3011 <aman.bhandari.3011@gmail.com>
2023-08-10 12:59:33 +0530

    manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,12 +1,27 @@
 const User = require('../models/user');
 
 
-module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
-}
+module.exports.profile = async function(req, res) {
+    try {
+        if (req.cookies.user_id) {
+            const user = await User.findById(req.cookies.user_id).exec();
 
+            if (user) {
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    user: user
+                });
+            }
+
+            return res.redirect('/users/sign-in');
+        } else {
+            return res.redirect('/users/sign-in');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+        return res.status(500).json({ message: 'Server Error' });
+    }
+};
 
 // render the sign up page
 module.exports.signUp = function(req, res){
@@ -48,6 +63,26 @@ module.exports.create = async function(req, res) {
 };
 
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
-    // TODO later
+module.exports.createSession = async function(req, res){
+
+   try{
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+        if(existingUser.password != req.body.password)
+        {
+            return res.redirect('back');
+        }
+
+        res.cookie('user_id' ,existingUser.id)
+        return res.redirect('/users/profile');
+    }
+    else{
+        return res.redirect('back');
+    }
+   }
+   catch(err)
+   {
+        console.error('Error:', err);
+        return res.status(500).json({ message: 'Server Error' });
+   }
 }
