c13a6d58a91b06a291a576ad3a60b18adb4f6714
amanb3011 <aman.bhandari.3011@gmail.com>
2023-08-14 12:52:51 +0530

    posts displayed on home

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,5 +1,18 @@
-module.exports.home = function(req,res){
+const Post = require('../models/post');
+
+module.exports.home = async function(req,res){
+   try{
+   const posts = await Post.find({}).populate('user');
+   if(posts)
+   {
     return res.render('home',{
-        title : "home"
+        title : "Codiel | home",
+        posts : posts
     });
-}
+    }
+    }
+    catch(err)
+    {
+        console.log('error');
+    }
+    }
